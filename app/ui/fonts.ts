3a7be7a8c15dbc7b1c92df8eeae4d4b45db546e8
Tuyishimire-lab <tuyishime1angel@gmail.com>
2024-08-02 09:10:07 +0200

    Initial commit of the Next.js dashboard project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ 
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter',
+
+});
+export const lusitana = Lusitana({subsets:['latin'], weight:['400', '700']})
